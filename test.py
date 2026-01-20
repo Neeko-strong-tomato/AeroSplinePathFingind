5c51f895ac1d91875ef2c49adfc3833d94fb5b26
antony <ant.thiery@free.fr>
2026-01-20 15:18:09 +0100

    master chiasse

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
 import numpy as np
-
 import trimesh
+
+mesh = trimesh.load_mesh("test_part.stl")
+mesh.apply_translation(-mesh.centroid)
+mesh.apply_scale(1.0 / mesh.scale)
+
+V = mesh.vertices
+path = []
+for k in range (1000):
+    path.append(trimesh.load_path(np.array([V[k], V[k+1]])))
+
+scene = trimesh.Scene()
+for line in path:
+    scene.add_geometry(line)
+scene.add_geometry(mesh)
+
+scene.show()
